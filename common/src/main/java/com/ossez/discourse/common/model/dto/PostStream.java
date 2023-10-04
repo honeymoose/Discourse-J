b911e962d8623d017e3181fbb31853ea2dece0df
YuCheng Hu <huyuchengus@gmail.com>
2023-10-04 08:07:28 -0400

    ZCH-18 新增 2 个对象用于处理 Topic 的返回数据

---
+++ common/src/main/java/com/ossez/discourse/common/model/dto/PostStream.java
@@ -0,0 +1,30 @@
+package com.ossez.discourse.common.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PostStream implements Serializable {
+    private static final long serialVersionUID = 7382606905010995326L;
+    private List<Post> posts;
+    private List<Long> stream;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Long> getStream() {
+        return stream;
+    }
+
+    public void setStream(List<Long> stream) {
+        this.stream = stream;
+    }
+}
+
+
